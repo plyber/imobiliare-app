d5be1053507544738fa43c14d8c239e08e61cbf3
Oliviu <dienes.oliviu@gmail.com>
2023-08-29 22:49:39 +0300

    Navbar styling and template adjustments

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -5,38 +5,37 @@ import {map, Observable} from "rxjs";
 import {AuthService} from "./auth.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class DataStorageService {
-    private listingsRef: AngularFireList<Listing>;
+  private listingsRef: AngularFireList<Listing>;
 
-    constructor(
-        private db: AngularFireDatabase,
-        private authService: AuthService
-    ) {
-        this.listingsRef = db.list<Listing>('/listings');
+  constructor(
+    private db: AngularFireDatabase,
+  ) {
+    this.listingsRef = db.list<Listing>('/listings');
 
-    }
+  }
 
-    addListing(property: Listing) {
-        console.log(property)
-        return this.listingsRef.push(property);
-    }
+  addListing(property: Listing) {
+    console.log(property)
+    return this.listingsRef.push(property);
+  }
 
-    getListings() {
-        return this.listingsRef.snapshotChanges().pipe(
-            map(actions =>
-                actions.map(action => {
-                    const data = action.payload.val();
-                    const id = action.key;
-                    return {id, ...data} as Listing;
-                })
-            )
-        );
-    }
-    getListingByID(id: string): Observable<Listing> {
-        console.log('test', 'listings/' + id);
-        return this.db.object<Listing>('listings/' + id).valueChanges();
-    }
+  getListings() {
+    return this.listingsRef.snapshotChanges().pipe(
+      map(actions =>
+        actions.map(action => {
+          const data = action.payload.val();
+          const id = action.key;
+          return {id, ...data} as Listing;
+        })
+      )
+    );
+  }
+  getListingByID(id: string): Observable<Listing> {
+    console.log('test', 'listings/' + id);
+    return this.db.object<Listing>('listings/' + id).valueChanges();
+  }
 
 }
